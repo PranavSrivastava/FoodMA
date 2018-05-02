e6468d3f95b3ae556cb037ecb191a381c13e6468
Om <pranavsrivastava453@gmail.com>
2018-05-02 22:07:44 +0530

    After Navigation Drawer

--- app/src/main/java/com/example/pranavsrivastava/fma1/List1.java
+++ app/src/main/java/com/example/pranavsrivastava/fma1/List1.java
@@ -10,7 +10,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -39,7 +43,7 @@ import static android.R.attr.tag;
  * Created by Pranav Srivastava on 03-Mar-18.
  */
 
-public class List1 extends AppCompatActivity implements View.OnClickListener
+public class List1 extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener
     {
     private Toolbar toolbar;
     private FirebaseAuth mAuth;
@@ -86,8 +90,30 @@ public class List1 extends AppCompatActivity implements View.OnClickListener
             }
         fab.setOnClickListener(this);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
 
     }
+
+
+
+        @Override
+        public void onBackPressed() {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         mAuth = FirebaseAuth.getInstance();
@@ -224,6 +250,31 @@ public class List1 extends AppCompatActivity implements View.OnClickListener
             ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
             return cm.getActiveNetworkInfo() != null;
+        }
+
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            if (id == R.id.nav_camera) {
+                // Handle the camera action
+            } else if (id == R.id.nav_gallery) {
+
+            } else if (id == R.id.nav_slideshow) {
+
+            } else if (id == R.id.nav_manage) {
+
+            } else if (id == R.id.nav_share) {
+
+            } else if (id == R.id.nav_send) {
+
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
         }
 
     }
